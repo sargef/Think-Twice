ea55b7a77d2e67656458e73f454105991366ea1b
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-30 16:42:51 +0930

    Update index.js

--- index.js
+++ index.js
@@ -183,6 +183,19 @@ const CancelAndStopIntentHandler = {
   },
 };
 
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
