b3bc604cc52bee233eb4164b399989f0ed24d943
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-07-14 13:08:50 +0930

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,20 @@
 
 const Alexa = require('ask-sdk-core');
 const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter'); 
+const input = require('./input');
 
 const VIDEO_URLS = {
-  "OneRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/School+Swoop/SchoolSwoopWithBackEd.mp4",
-  "TwoRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/PartyTrick/PartyTrickWithBackEd.mp4",
+  "Battle of Plassey": "https://thinktwice3.s3-eu-west-1.amazonaws.com/School+Swoop/SchoolSwoopWithBackEd.mp4",
+  "TwoRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/PartyTrickSSVideo.mp4",
   "OneRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ConcertMania/ConcertManiaWithBackEd.mp4",
-  "TwoRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ShoppingShark/ShoppingSharkWithBackEd.mp4",
-  "OneRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/PlanetaryAttack/PlanetaryAttackWithBackEd.mp4",
-  "TwoRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/DiaryDash/DiaryDashWithBackEd.mp4",
-  "OneRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ChemistryQuiz/ChemistryQuizWithBackEd.mp4",
+  "TwoRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/ShoppingSharkSSVideo.mp4",
+  "OneRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/PlanetaryAttackSSVideo.mp4",
+  "TwoRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/DiaryDashSSVideo.mp4",
+  "OneRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/ChemistryQuizSSVideo.mp4",
   "TwoRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ApptAppeal/ApptAppealWithBackEd.mp4",
   "RoundGold": "https://thinktwice3.s3-eu-west-1.amazonaws.com/MedalsWithBack/GoldMedalWithBack.mp4",
-  "RoundSilver": "https://thinktwice3.s3-eu-west-1.amazonaws.com/MedalsWithBack/SilverMedalWithBack.mp4",
-  "RoundBronze": "https://thinktwice3.s3-eu-west-1.amazonaws.com/MedalsWithBack/BronzeMedalWithBack.mp4",
+  "RoundSilver": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SilverMedalNNVid.mp4",
+  "RoundBronze": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/BronzeMedalNNVid.mp4",
   "Home": "https://thinktwice3.s3-eu-west-1.amazonaws.com/WelcomeAndStartReprompt/WelcomeWithBackEd.mp4",
   "Instructions": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Instructions/InstructionsWithBackEd.mp4",
   "YesPlay": "https://thinktwice3.s3-eu-west-1.amazonaws.com/YesPlay/YesPlayWithBack.mp4",
@@ -24,27 +25,24 @@ const VIDEO_URLS = {
     };
 
 const AUDIO_URLS = {
-  "OneRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/School+Swoop/schooswoop1.mp3" + "https://thinktwice3.s3-eu-west-1.amazonaws.com/School+Swoop/schoolswoop2.mp3",
-  "TwoRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/PartyTrick/Round1PartyTrickEntireSounds.mp3",
-  "OneRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ConcertMania/Round2ConcertManiaAllSounds.mp3",
-  "TwoRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ShoppingShark/Round2ShoppingSharkAll.mp3",
-  "OneRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/PlanetaryAttack/Round3PlanetaryAttackAll.mp3",
-  "TwoRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/DiaryDash/Round3DiaryDashPart1all.mp3"+ "https://thinktwice3.s3-eu-west-1.amazonaws.com/DiaryDash/Round3DiaryDashpart2all.mp3",
-  "OneRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ChemistryQuiz/Round4ChemistryQuizAll.mp3",
-  "TwoRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/ApptAppeal/Round4ApptAppealPart1All.mp3" + "https://thinktwice3.s3-eu-west-1.amazonaws.com/ApptAppeal/Round4ApptAppealAllpart2.mp3",
-  "RoundGold": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Medals/GoldMedalAll.mp3",
-  "RoundSilver": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Medals/SilverMedalAll.mp3",
-  "RoundBronze": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Medals/BronzeMedalAll.mp3",
-  "Home": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Vids/welcomeintroentiresounds.mp3",
-  "GameReprompt": "https://thinktwice3.s3-eu-west-1.amazonaws.com/WelcomeAndStartReprompt/BeginReprompt.mp3",
-  "Instructions": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Instructions/Instructions.mp3",
-  "YesPlay": "https://thinktwice3.s3-eu-west-1.amazonaws.com/YesPlay/StartYes.mp3",
-  "Stop": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Stop/StopCancel.mp3",
-  "Help": "https://thinktwice3.s3-eu-west-1.amazonaws.com/Help/Help.mp3",
-  "HelpReprompt":"https://thinktwice3.s3-eu-west-1.amazonaws.com/WelcomeAndStartReprompt/BeginReprompt.mp3"
+  "OneRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/schoolswoop1nn.mp3" + "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/schoolswooptwonn.mp3",
+  "TwoRoundOne": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/partytrickss.mp3",
+  "OneRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/concertmaniann.mp3",
+  "TwoRoundTwo": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/shoppinsharkss.mp3",
+  "OneRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/planetaryattackss.mp3",
+  "TwoRoundThree": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/diarydash1nn.mp3"+ "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SS/DiaryDashTwoSS.mp3",
+  "OneRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/chemistryquiz1nn.mp3" + "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/chemistryquiz2nn.mp3",
+  "TwoRoundFour": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/apptappeal1nn.mp3" + "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/appappeal2nn.mp3",
+  "RoundGold": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/goldmedalnn.mp3",
+  "RoundSilver": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/SilverMedalNNNN.mp3",
+  "RoundBronze": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/BronzeMedalNNNN.mp3",
+  "Home": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/welcomenn.mp3",
+  "Instructions": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/instructionsnn.mp3",
+  "YesPlay": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/yesstartnn.mp3",
+  "Stop": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/stopnn.mp3",
+  "Help": "https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/helpnn.mp3",
+  "HelpReprompt":"https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/helprepromptnn.mp3"
   };
-
-const input = require('./input');
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -156,7 +154,7 @@ const PlayIntentHandler = {
 
     return request.type === 'IntentRequest' && 
       (request.intent.name === 'PlayIntent' || 
-        (startGame && request.intent.name === 'AMAZON.YesIntent'));
+        (startGame && request.intent.name === 'YesIntent'));
     },
     async handle(handlerInput) {
     let { requestEnvelope, attributesManager} = handlerInput;
@@ -167,14 +165,15 @@ const PlayIntentHandler = {
     let speechText = '';
     let reprompt = '';
     let speechArr = [];
-    let VIDEOURL = [];
+    let VIDEO_URL = [];
+  
+    
 
-    if(level>input.INPUT_DATA.length){
+     if(level>input.INPUT_DATA.length){
       speechArr.push(input.COMPLETED_ALL_LEVELS);
       speechArr.push(input.COMPLETED_ALL_LEVELS_UPDATE);
       speechArr.push();
       speechText = await convertArrayToSpeech(speechArr);
-      
       sessionAttributes.questionCount = 0;
       sessionAttributes.clueCount=0;
       sessionAttributes.endedSessionCount += 1;
@@ -201,21 +200,20 @@ const PlayIntentHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(reprompt)
-      .withSimpleCard('Think Twice')
       .getResponse();
     }else{
       let round = input.INPUT_DATA[level-1];
       if(sessionAttributes.gameState === 'ENDED' || sessionAttributes.gameState === 'CLUES' || (sessionAttributes.questionCount == 0)){
         let question = 0; 
-        speechArr.push(`Your Level ${level.toString()} is category ${round.Round}`);
-        speechArr.push(`Lets get started with ${round.Round}`);
+        speechArr.push(`Your Level ${level.toString()} category is ${round.Round}`);
+        speechArr.push(`Lets get started`);
         speechText = await convertArrayToSpeech(speechArr)+await convertArrayToSpeech(round.Description);
-       
+  
         speechArr = [];
         speechArr.push(input.START_ANSWERS_MESSAGE_PROMPT);
         speechArr.push(`Let's now go through the answers from ${round.Round}`);
         speechArr.push(`You can ask me for a clue if you need to answer the question`);
-        speechArr.push(`Here is your question. ${round.Subquestion[question].Question}`);
+        speechArr.push(`${round.Subquestion[question].Question}`);
         
         speechText = speechText + await convertArrayToSpeech(speechArr);
         reprompt = await convertArrayToSpeech(input.GAME_PLAY_REPROMPT);
@@ -233,27 +231,27 @@ const PlayIntentHandler = {
             sessionAttributes.clueCount=0;
             speechArr.push(input.ANSWER_CORRECT_MESSAGE);
             let questionCount = sessionAttributes.questionCount;
-  
-            if(questionCount <= 4){
-              let question = Math.floor(Math.random() * (round.Subquestion.length - 0) + 0);
+            
+          if (questionCount <= 5){
+          let question = Math.floor(Math.random() * (round.Subquestion.length - 0) + 0);
       
               speechArr.push(input.NEXT_QUESTION_PROMPT);
-              speechArr.push(`Here is your question. ${round.Subquestion[question].Question}`);
+              speechArr.push(` ${round.Subquestion[question].Question}`);
         
               sessionAttributes.question = question;
               sessionAttributes.questionCount += 1;
             }else{
               speechArr.push(input.ROUND_COMPLETE_PROMPT);
-              speechArr.push("your score is "+sessionAttributes.score);
+              speechArr.push("your score is "+sessionAttributes.score+ " points");
       
               if(sessionAttributes.score > 50){
                 level += 1;
                 let badge = (Math.floor((sessionAttributes.score/20)-2));
                 sessionAttributes.badge += badge;
                 
-                speechArr.push("You have progressed to "+level+" level.");
-                speechArr.push("and You have won a "+badge+ " medal, your total medal count is "+sessionAttributes.badge);
-                speechArr.push("Would you like to play level "+level+"?");
+                speechArr.push("<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/magiccash/cash-machine.mp3' />" + "You have progressed to level"+ level);
+                speechArr.push("and You have won " +badge+ " medals, your total medal count is"+sessionAttributes.badge);
+                speechArr.push("Would you like to play level " +level+ "?");
       
                 sessionAttributes.level = level;
               }else{
@@ -264,11 +262,11 @@ const PlayIntentHandler = {
               sessionAttributes.questionCount = 0;
             }
           } else {
-            speechArr.push("Oh oh. "+answer+" is incorrect");
+            speechArr.push("sorry, "+answer+" is incorrect" + "<audio src='https://s3-eu-west-1.amazonaws.com/familyfeuding/mattswordmasterBOHBOH.mp3' />");
             speechArr = speechArr.concat(await getClue(attributesManager, sessionAttributes));
           }
         }else{
-          speechArr.push("Alright, lets come back.");
+          speechArr.push("<voice name='Matthew'>'Alright, lets try again.'</voice>");
           speechArr.push(`Here is your question. ${round.Subquestion[question].Question}`);
         }
         speechText = await convertArrayToSpeech(speechArr);
@@ -295,7 +293,6 @@ const PlayIntentHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(reprompt)
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
@@ -333,7 +330,6 @@ const CluesIntentHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt()
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
@@ -348,7 +344,7 @@ const HelpIntentHandler = {
           && handlerInput.requestEnvelope.request.arguments[0] === 'help');
   },
   handle(handlerInput) {
-    const speechText = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/Help/Help.mp3'/> ";
+    const speechText = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/helpnn.mp3'/> ";
     const reprompt = "";
 
     if (supportsAPL(handlerInput)) {
@@ -369,7 +365,6 @@ const HelpIntentHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(reprompt)
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
@@ -384,8 +379,8 @@ const InstructionsIntentHandler = {
           && handlerInput.requestEnvelope.request.arguments[0] === 'instructions');
   },
   handle(handlerInput) {
-    const speechText = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/Instructions/Instructions.mp3'/>";
-    const InstructionsReprompt = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/WelcomeAndStartReprompt/BeginReprompt.mp3' />";
+    const speechText = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/instructionsnn.mp3'/>";
+    const InstructionsReprompt = "<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/helprepromptnn.mp3' />";
 
     if (supportsAPL(handlerInput)) {
       handlerInput.responseBuilder
@@ -404,7 +399,6 @@ const InstructionsIntentHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(InstructionsReprompt)
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
@@ -461,10 +455,22 @@ const CancelAndStopIntentHandler = {
     }
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
+
+//const StartOverRequestHandler = {
+//  canHandle(handlerInput) {
+//    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+//    && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartoverIntent');
+//
+//    handle(handlerInput) {
+//      if (questionCount => 1){
+//    console.log (questionCount(== null));
+////      }
+//    return handlerInput.responseBuilder.getResponse();
+//  },
+//};
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
@@ -504,7 +510,6 @@ const ErrorHandler = {
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Think Twice')
       .getResponse();
   },
 };
@@ -516,7 +521,7 @@ const FallbackHandler = {
     const request = handlerInput.requestEnvelope.request;
     return request.type === 'IntentRequest' &&
       (request.intent.name === 'AMAZON.FallbackIntent' ||
-        request.intent.name === 'RulesIntent' ||
+        request.intent.name === 'InstructionsIntent' ||
         request.intent.name === 'PlayIntent' ||
         request.intent.name === 'CluesIntent');
   },
@@ -585,25 +590,25 @@ async function getClue(attributesManager, sessionAttributes){
     let questionCount = sessionAttributes.questionCount;
     speechArr.push(input.NO_CLUES_LEFT);
 
-    if(questionCount < 4){
-      let question = Math.floor(Math.random() * (round.Subquestion.length - 0) + 0);
-
+   if (questionCount < 5){
+    let question = Math.floor(Math.random() * (round.Subquestion.length - 0) + 0);
+      
       speechArr.push(input.NEXT_QUESTION_PROMPT);
-      speechArr.push(`Here is your question. ${round.Subquestion[question].Question}`);
+      speechArr.push(`${round.Subquestion[question].Question}`);
 
       sessionAttributes.question = question;
       sessionAttributes.questionCount += 1;
     }else{
       speechArr.push(input.FOUR_QUESTIONS_COMPLETE);
-      speechArr.push("your score is "+sessionAttributes.score);
+      speechArr.push("your score is "+sessionAttributes.score+ " points");
 
       if(sessionAttributes.score > 50){
         level += 1;
         let badge = (Math.floor((sessionAttributes.score/20)-2));
         sessionAttributes.badge += badge;
         
-        speechArr.push("You have moved to "+level);
-        speechArr.push("and You have won "+badge+ " badge, your total badge is "+sessionAttributes.badge);
+        speechArr.push("<audio src='https://thinktwice3.s3-eu-west-1.amazonaws.com/NewLouderSounds/NN/magiccash/cash-machine.mp3' />" + "You have progressed to "+level+"level.");
+        speechArr.push("and You have won " +badge+ "medals, your total medal count is "+sessionAttributes.badge);
         speechArr.push("Do you want to play level "+level+"?");
 
         sessionAttributes.level = level;
